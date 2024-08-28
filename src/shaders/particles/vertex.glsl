d809187bae98c902adbc22b37dab413c71c085c1
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-08-28 21:02:00 +0530

    added a simplex noise for transition (looks better than linear one)

--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -4,11 +4,26 @@ uniform float uProgress;
 
 attribute vec3 aPositionTarget;
 
+varying vec3 vColor;
+
+#include ../includes/simplexNoise3d.glsl
+
 void main()
 {
     // float progress = 0.5;
 
-    vec3 mixedPosition = mix(position, aPositionTarget, uProgress);
+    float noiseOrigin = simplexNoise3d(position * 0.2);
+    float noiseTarget = simplexNoise3d(aPositionTarget * 0.2);
+    float noise = mix(noiseOrigin, noiseTarget, uProgress);
+    noise = smoothstep(-1.0, 1.0, noise);
+
+    float duration = 0.4;
+    float delay = (1.0 - duration) * noise;
+    float end = delay + duration;
+    float progress = smoothstep(delay, end, uProgress);
+
+
+    vec3 mixedPosition = mix(position, aPositionTarget, progress);
 
     // Final position
     vec4 modelPosition = modelMatrix * vec4(mixedPosition, 1.0);
@@ -19,4 +34,8 @@ void main()
     // Point size
     gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+
+
+    //varyings
+    vColor = vec3(noise);
 }
