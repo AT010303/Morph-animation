51beaa000a7c02d55617fc1bf57d572fa05ace63
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-08-29 20:05:41 +0530

    fixed frame stutter

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,12 +3,12 @@ import { Canvas } from '@react-three/fiber';
 import {
     Bloom,
     DepthOfField,
-    EffectComposer
-    // Noise,
-    // TiltShift2
+    EffectComposer,
+    Noise,
+    TiltShift2
 } from '@react-three/postprocessing';
 import { useControls } from 'leva';
-// import { BlendFunction } from 'postprocessing';
+import { BlendFunction } from 'postprocessing';
 import { Perf } from 'r3f-perf';
 
 import Sphere from './Sphere';
@@ -27,7 +27,7 @@ const Experience = () => {
                 }}
             >
                 <color attach="background" args={[controls.backgroundColor]} />
-                <fog attach="fog" args={['#f0f0f0', 0, 20]} />
+                {/* <fog attach="fog" args={['#f0f0f0', 0, 20]} /> */}
                 <Perf position={'top-left'} />
                 <OrbitControls enabled={false} enableZoom={false} />
                 <ScrollControls pages={2} damping={0.5}>
@@ -37,16 +37,16 @@ const Experience = () => {
                     {/* <TiltShift2 blur={0.03} /> */}
                     <Bloom
                         intensity={0.7}
-                        mipmapBlur
+                        // mipmapBlur
                         luminanceThreshold={0.4}
                         luminanceSmoothing={0.25}
                     />
-                    {/* <Noise premultiply blendFunction={BlendFunction.ADD} /> */}
-                    <DepthOfField
+                    <Noise premultiply blendFunction={BlendFunction.ADD} />
+                    {/* <DepthOfField
                         focusDistance={1.25} // where to focus
                         focalLength={0.9} // focal length
                         bokehScale={2} // bokeh size
-                    />
+                    /> */}
                 </EffectComposer>
             </Canvas>
         </>
